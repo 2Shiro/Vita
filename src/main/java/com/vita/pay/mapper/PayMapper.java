c349d578c4994d37908836dd82e4225248e56813
2Shiro <animetales@naver.com>
2024-06-14 22:01:21 +0900

    장바구니 수량 변동 개발중

--- src/main/java/com/vita/pay/mapper/PayMapper.java
+++ src/main/java/com/vita/pay/mapper/PayMapper.java
@@ -3,6 +3,7 @@ package com.vita.pay.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.vita.pay.domain.BasketVo;
 import com.vita.pay.domain.ImgsVo;
@@ -13,11 +14,21 @@ import com.vita.pay.domain.ProdVo;
 public interface PayMapper {
 
 	// 장바구니 목록
-	List<BasketVo> getBasketList(Long userId);
+	List<BasketVo> getBasketList(Long id);
 	
 	// 장바구니 상품에 대한 정보
 	ProdVo getProd(int pro_id);
 	ImgsVo getImg(int pro_id);
 	MakeVo getMake(int make_id);
+
+	// 장바구니 특정 상품 정보
+	BasketVo getBasket(Long basket_id);
+	// 장바구니 상품 수량 변경
+	void updateBasketCount(BasketVo basketvo);
+
+	
+
+	
+
 
 }
