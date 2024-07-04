37973945389537cb4dced0534e10b1d4c87c9480
2Shiro <animetales@naver.com>
2024-07-04 17:19:43 +0900

    환불 신청 완료

--- src/main/java/com/vita/user/mapper/UsersMapper.java
+++ src/main/java/com/vita/user/mapper/UsersMapper.java
@@ -11,15 +11,17 @@ import com.vita.user.domain.PayVo;
 @Mapper
 public interface UsersMapper {
 
-	List<GoodsVo> getGoodsList(Long id);
+    List<GoodsVo> getGoodsList(Long id);
 
-	// 로그인 회원의 구매 상품 조회
-	List<PayVo> getPay(Long id);
+    // 로그인 회원의 구매 상품 조회
+    List<PayVo> getPay(Long id);
 
-	// 구매 고유번호 조회
-	PayVo getIdentity(int pay_id);
+    // 구매 고유번호 조회
+    PayVo getIdentity(int pay_id);
 
     // 환불신청
     void requestRefund(@Param("payId") int payId, @Param("refundReason") String refundReason) throws Exception;
 
+    // 환불 신청 여부 확인
+    boolean isRefundRequested(@Param("payId") int payId);
 }
